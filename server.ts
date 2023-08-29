1c683e0420c6fb8686315fd7282f14277cd9b4ca
Markus Korn <markus.korn@gmail.com>
2023-08-29 08:21:29 +0200

    refactor(server.ts): encapsulate runningGames in a class for better structure and readability feat(server.ts): add start, stop, next methods to RunningGames class to manage game lifecycle fix(server.ts): replace direct map operations with RunningGames class methods for consistency and encapsulation

--- server.ts
+++ server.ts
@@ -1,7 +1,39 @@
 import { serve } from 'bun';
 import { GameOfLifeState } from './game';
 
-const runningGames: Map<number, GameOfLifeState> = new Map();
+class RunningGames {
+  private _games: Map<number, GameOfLifeState> = new Map();
+
+  constructor() {}
+
+  has(gameId: number): boolean {
+    return this._games.has(gameId);
+  }
+
+  start(gameId: number) {
+    const g = GameOfLifeState.createRandom(20, 20);
+    this._games.set(gameId, g);
+    console.log('>>> game', gameId, 'started'); 
+  }
+
+  next(gameId: number): GameOfLifeState | null {
+    const oldState = this._games.get(gameId)!;
+    const newState = oldState.next();
+    if (GameOfLifeState.compare(oldState, newState)) {
+      this.stop(gameId)
+      return null
+    }
+    this._games.set(gameId, newState!);
+    return newState
+  }
+
+  stop(gameId: number) {
+    this._games.delete(gameId);
+    console.log('>>> game', gameId, 'stopped');
+  }
+}
+
+const runningGames = new RunningGames();
 
 type WebSocketData = {
   gameId: number;
@@ -16,15 +48,11 @@ serve<WebSocketData>({
   websocket: {
     open: async (ws) => {
       console.log('Client connected, starting new game:', ws.data.gameId);
-      const g = GameOfLifeState.createRandom(20, 20);
-      runningGames.set(ws.data.gameId, g);
+      runningGames.start(ws.data.gameId);
 
       while (runningGames.has(ws.data.gameId)) {
-        const oldState = runningGames.get(ws.data.gameId)!;
-        const newState = oldState.next();
-        runningGames.set(ws.data.gameId, newState!);
-
-        if (GameOfLifeState.compare(oldState, newState!)) {
+        const newState = runningGames.next(ws.data.gameId);
+        if (newState === null) {
           ws.send(`<div id="game-state" hx-swap-oob="true" class="m-2 max-w-min">GAME OVER</div>`);
           break;
         } else {
@@ -40,7 +68,7 @@ serve<WebSocketData>({
     },
     close: (ws) => {
       console.log('Client disconnected, stopping game:', ws.data.gameId);
-      runningGames.delete(ws.data.gameId);
+      runningGames.stop(ws.data.gameId);
     },
   },
   fetch(req, server) {
